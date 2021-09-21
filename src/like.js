b56f0f6ade819feb789dd9b0392a33760d81df9e
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-09-21 17:17:29 -0400

    created handleClick method

--- src/like.js
+++ src/like.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react'
 export default class like extends Component { 
 
     state = { 
-        toggle: false
-    } 
+        toggled: false
+    }  
+
+    handleClick = () => {
+        this.setState(prevState => {
+            return {toggled: !prevState.toggled}
+        })
+    }
 
 
     render() {
